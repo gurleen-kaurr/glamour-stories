14af997e373b3981de9a58ec3e4159ab80a5687d
gurleen-kaurr <gurleen.kaurrrrrr@gmail.com>
2023-10-20 23:45:33 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router , Routes ,Route } from "react-router-dom";
 import Nav from "./nav/Nav.js";
 const App = () => {
-  return<></>
- 
-};
+  return<>
+  <Nav/>
+  </>
+ };
 
 export default App;
---
+++ src/nav/Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import astronautHelmet from "../assets"
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>This is the Nav page </div>
+    )
+  }
+}
